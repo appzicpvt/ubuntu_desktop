542603eef94f6b96bcff3f478f5b05a5384954cd
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2023-01-17 16:23:47 +0530

    remove freesarif fonts

--- 22.04/set_sinhala_font/set_font.sh
+++ 22.04/set_sinhala_font/set_font.sh
@@ -3,3 +3,4 @@ FONT=bashitha
 sudo rm /usr/share/fonts/truetype/sinhala/*.ttf
 sudo cp ./fonts/malithi.ttf /usr/share/fonts/truetype/sinhala/
 sudo cp ./fonts/malithi.ttf /usr/share/fonts/truetype/freefont/
+sudo cd /usr/share/fonts/truetype/ && sudo rm FreeSerif.ttf FreeSerifBold.ttf FreeSerifItalic.ttf FreeSerifBoldItalic.ttf

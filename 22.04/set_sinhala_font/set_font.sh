2be2685dd73634960b16f0024fc82f4064b7bf32
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2023-01-17 16:28:16 +0530

    add another way to delete free sarif

--- 22.04/set_sinhala_font/set_font.sh
+++ 22.04/set_sinhala_font/set_font.sh
@@ -3,4 +3,6 @@ FONT=bashitha
 sudo rm /usr/share/fonts/truetype/sinhala/*.ttf
 sudo cp ./fonts/malithi.ttf /usr/share/fonts/truetype/sinhala/
 sudo cp ./fonts/malithi.ttf /usr/share/fonts/truetype/freefont/
-sudo cd /usr/share/fonts/truetype/ && sudo rm FreeSerif.ttf FreeSerifBold.ttf FreeSerifItalic.ttf FreeSerifBoldItalic.ttf
+
+# remove free serif fonts
+sudo rm /usr/share/fonts/truetype/freefont/{FreeSerif.ttf,FreeSerifBold.ttf,FreeSerifItalic.ttf,FreeSerifBoldItalic.ttf}

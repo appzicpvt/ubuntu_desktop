dcb7884b4904359472795762daff7f4b180caa42
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2023-01-17 16:06:10 +0530

    copy font to free font

--- 22.04/set_sinhala_font/set_font.sh
+++ 22.04/set_sinhala_font/set_font.sh
@@ -2,3 +2,4 @@ FONT=bashitha
 
 sudo rm /usr/share/fonts/truetype/sinhala/*.ttf
 sudo cp ./fonts/malithi.ttf /usr/share/fonts/truetype/sinhala/
+sudo cp ./fonts/malithi.ttf /usr/share/fonts/truetype/freefont/
